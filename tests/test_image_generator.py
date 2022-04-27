ae614dea3def4cbc51f0d913e0d456ee467a89f2
SEAFHMC <d1366160279@gmail.com>
2022-04-27 22:26:46 +0800

    摸了

--- tests/test_image_generator.py
+++ tests/test_image_generator.py
@@ -1,7 +1,7 @@
 from loguru import logger
 from image_generator._api_result.request import get_user_b30, get_user_recent
 from image_generator.AUA.schema.basemodel import Base
-from image_generator.AUA.schema.api.v5 import AccountInfo, SongScore, UserRecent
+from image_generator.AUA.schema.api.v5 import AccountInfo, SongScore, UserRecent, UserBest30
 from image_generator.utils import get_song_info
 
 from typing import Dict, List, Optional
