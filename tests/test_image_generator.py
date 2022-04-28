706efe7378d47b72bcb191c68021c6d9fa348af1
SEAFHMC <d1366160279@gmail.com>
2022-04-28 22:52:37 +0800

    弃用slst

--- tests/test_image_generator.py
+++ tests/test_image_generator.py
@@ -1,10 +1,15 @@
+from image_generator._api_result.request import get_user_recent
 from image_generator.b30_image import draw_b30
+from image_generator.recent_image import draw_recent
 from image_generator.request import get_user_b30
+from image_generator.AUA import SongInfo, UserRecent, AccountInfo
+from image_generator.utils import DataText, draw_text, open_img, player_time_format, choice_ptt_background, StaticPath
+from PIL import Image
+from typing import Dict
 
 
-data = get_user_b30()
+data = get_user_recent()
 
 
-
-res = draw_b30(data)
+res = draw_recent(data)
 res.show()
